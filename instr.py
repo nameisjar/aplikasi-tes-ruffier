2f62cc2e2a06d86bce915a16bbc07c38c684950c
Niko Muhamad Fajar <90367273+nameisjar@users.noreply.github.com>
2025-09-27 12:36:52 +0700

    add text result

--- instr.py
+++ instr.py
@@ -1,3 +1,11 @@
+
 text_title = 'tes ruffier app'
 win_width = 1000
 win_height = 600
+txt_res1 = "low. See your doctor right away!"
+txt_res2 = "satisfactory. See your doctor!"
+txt_res3 = "average. It may be worth seeing your doctor to get checked out."
+txt_res4 = "above average"
+txt_res5 = "high"
+txt_index = 'Roufier Index: '
+txt_workheart = 'Cardiac performance: '
