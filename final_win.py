b36ad46b0cbbb1c64eb90a773508970ada4d0327
Niko Muhamad Fajar <90367273+nameisjar@users.noreply.github.com>
2025-09-27 12:37:27 +0700

    add feat/result index

--- final_win.py
+++ final_win.py
@@ -1,31 +1,89 @@
+
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout
 from instr import *
 
+
 class FinalWin(QWidget):
-    def __init__(self):
+    def __init__(self, exp):
         super().__init__()
+        self.exp = exp
         self.set_appear()
         self.initUI()
-        self.connects()
         self.show()
         
     def set_appear(self):
         self.setWindowTitle(text_title)
         self.resize(win_width, win_height)
     
+    def results(self):
+        if self.exp.input_age < 7:
+            self.index = 0
+            return 'there is no data for this input_age'
+        self.index = (4 * (int(self.exp.input_tahap1) + int(self.exp.input_tahap2)+ int(self.exp.input_tahap3)) - 200) / 10
+        if self.exp.input_age == 7 or self.exp.input_age == 8:
+            if self.index >= 21:
+                return txt_res1
+            elif self.index < 21 and self.index >= 17:
+                return txt_res2
+            elif self.index < 17 and self.index >= 12:
+                return txt_res3
+            elif self.index < 12 and self.index >= 6.5:
+                return txt_res4
+            else:
+                return txt_res5
+        if self.exp.input_age == 9 or self.exp.input_age == 10:
+            if self.index >= 19.5:
+                return txt_res1
+            elif self.index < 19.5 and self.index >= 15.5:
+                return txt_res2
+            elif self.index < 15.5 and self.index >= 10.5:
+                return txt_res3
+            elif self.index < 10.5 and self.index >= 5:
+                return txt_res4
+            else:
+                return txt_res5
+        if self.exp.input_age == 11 or self.exp.input_age == 12:
+            if self.index >= 18:
+                return txt_res1
+            elif self.index < 18 and self.index >= 14:
+                return txt_res2
+            elif self.index < 14 and self.index >= 9:
+                return txt_res3
+            elif self.index < 9 and self.index >= 3.5:
+                return txt_res4
+            else:
+                return txt_res5
+        if self.exp.input_age == 13 or self.exp.input_age == 14:
+            if self.index >= 16.5:
+                return txt_res1
+            elif self.index < 16.5 and self.index >= 12.5:
+                return txt_res2
+            elif self.index < 12.5 and self.index >= 7.5:
+                return txt_res3
+            elif self.index < 7.5 and self.index >= 2:
+                return txt_res4
+            else:
+                return txt_res5
+        if self.exp.input_age >= 15:
+            if self.index >= 15:
+                return txt_res1
+            elif self.index < 15 and self.index >= 11:
+                return txt_res2
+            elif self.index < 11 and self.index >= 6:
+                return txt_res3
+            elif self.index < 6 and self.index >= 0.5:
+                return txt_res4
+            else:
+                return txt_res5
+            
     def initUI(self):
-        self.index = QLabel('Ruffier index: 0')
-        self.result = QLabel('cardiac performance: there is no data for this age')
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.index, alignment=Qt.AlignCenter)
-        self.layout.addWidget(self.result, alignment=Qt.AlignCenter)
-        
-        self.setLayout(self.layout)
+        self.work_text = QLabel(txt_workheart + self.results())
+        self.index_text = QLabel(txt_index + str(self.index))
+
+        self.layout_line = QVBoxLayout()
+        self.layout_line.addWidget(self.index_text, alignment = Qt.AlignCenter)
+        self.layout_line.addWidget(self.work_text, alignment = Qt.AlignCenter)        
+        self.setLayout(self.layout_line)
     
-    def next_click(self):
-        pass
-    
-    def connects(self):
-        pass
 
